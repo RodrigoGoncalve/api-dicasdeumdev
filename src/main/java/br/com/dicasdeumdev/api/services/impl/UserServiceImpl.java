ceae946a3509061366a2ec246bf9230acf4347bb
Rodrigo Gonçalves <rodrigo.goncalves@squadra.com.br>
2022-06-25 16:01:45 -0300

    endpoint create user

--- src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.dicasdeumdev.api.services.impl;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepositoy;
 import br.com.dicasdeumdev.api.services.UserServices;
 import br.com.dicasdeumdev.api.services.excepyions.ObjectNotFoundexception;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserServices {
     @Autowired
     private UserRepositoy userRepositoy;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepositoy.findById(id);
@@ -24,6 +29,11 @@ public class UserServiceImpl implements UserServices {
 
     public List<User> findAll(){
         return userRepositoy.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return userRepositoy.save(mapper.map(obj, User.class));
     }
 
 }
